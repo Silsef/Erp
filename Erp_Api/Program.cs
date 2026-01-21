0201345d75e0b942a96c5a374ae27d90bf818447
Urashy <berkanakin05@gmail.com>
2026-01-21 14:26:30 +0100

    correction jwt connexion

--- Erp_Api/Program.cs
+++ Erp_Api/Program.cs
@@ -1,17 +1,48 @@
+using Erp_Api.Mapper;
 using Erp_Api.Models.Entity;
 using Erp_Api.Models.Repository.Managers.Models_Managers;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
-using Erp_Api.Mapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Erp API", Version = "v1" });
+
+    // 1. Définir le schéma de sécurité (Bearer)
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Entrez un jeton JWT valide",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+
+    // 2. Ajouter l'exigence de sécurité globale
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 
 // Configuration de la base de données
 builder.Services.AddDbContext<ErpBdContext>(options =>
@@ -53,7 +84,12 @@ builder.Services.AddAuthentication(options =>
     {
         OnMessageReceived = context =>
         {
-            context.Token = context.Request.Cookies["access_token"];
+            var cookie = context.Request.Cookies["access_token"];
+
+            if (!string.IsNullOrEmpty(cookie))
+            {
+                context.Token = cookie;
+            }
             return Task.CompletedTask;
         }
     };
