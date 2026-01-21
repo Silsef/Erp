a10a069a065ec58d9f2c181975cff2a36420c045
Urashy <berkanakin05@gmail.com>
2026-01-21 12:13:42 +0100

    début arborescance menu

---
+++ Erp_Api/Models/Entity/Tables/Entitees/Tache.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Erp_Api.Models.Entity.Tables.Entitees
+{
+    [Table("t_e_tache_tac")]
+    public class Tache
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("tac_id")]
+        public int Id { get; set; }
+    }
+}
